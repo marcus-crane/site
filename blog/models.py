a11b398b9710a206f1bc9230cc8a5e7cfee6464e
Marcus Crane <marcus@thingsima.de>
2017-10-23 19:24:33 +1300

    Created blog model

---
+++ blog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            'Creation date',
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            'Publication date',
+            blank=True, null=True)
+    draft = models.BooleanField(
+            'Draft post?',
+            default=True)
+
+    def publish(self):
+        self.draft = False
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
