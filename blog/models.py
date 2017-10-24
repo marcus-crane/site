64586ef36ad38a86d9da879854e7eff595f943f9
Marcus Crane <marcus@thingsima.de>
2017-10-25 00:19:03 +1300

    Simplified dates and automated slugification

--- blog/models.py
+++ blog/models.py
@@ -1,24 +1,27 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=40)
+    slug = models.SlugField(max_length=40, blank=True)
     text = models.TextField()
-    created_date = models.DateTimeField(
-            'Creation date',
-            default=timezone.now)
-    published_date = models.DateTimeField(
+    date = models.DateTimeField(
             'Publication date',
             blank=True, null=True)
     draft = models.BooleanField(
             'Draft post?',
             default=True)
 
+    # When saving, automatically generate the slug from the title
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
     def publish(self):
         self.draft = False
-        self.published_date = timezone.now()
+        self.date = timezone.now()
         self.save()
 
     def __str__(self):
--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,8 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(
-                published_date__lte=timezone.now()
-            ).order_by('-published_date')
+                date__lte=timezone.now()
+            ).order_by('-date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, slug):
