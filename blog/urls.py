a36e2aa70b21368fdf4791c9c6856ebfb5344dfc
Marcus Crane <marcus@thingsima.de>
2017-10-24 23:33:39 +1300

    Added auto escaping for post text

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
+app_name = 'blog'
 urlpatterns = [
-  url(r'^$', views.post_list, name='post_list'),
+  path('', views.post_list, name='post_list'),
+  path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
