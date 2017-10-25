bf7370cf0372911a319cd45b52e781f9f94d67ee
Marcus Crane <marcus@thingsima.de>
2017-10-25 13:52:34 +1300

    Genericised views

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-  path('', views.post_list, name='post_list'),
-  path('<slug:slug>/', views.post_detail, name='post_detail'),
+  path('', views.PostView.as_view(), name='post_list'),
+  path('<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from django.views import generic
 
 from .models import Post
 
-def post_list(request):
-    posts = Post.objects.filter(
-                date__lte=timezone.now()
-            ).order_by('-date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+class PostView(generic.ListView):
+  template_name = 'blog/post_list.html'
+  context_object_name = 'posts'
 
-def post_detail(request, slug):
-    post = get_object_or_404(Post, slug=slug)
-    return render(request, 'blog/post_detail.html', {'post': post})
+  def get_queryset(self):
+    """ Order posts by date in descending order."""
+    return Post.objects.filter(date__lte=timezone.now()).order_by('-date')[:3]
+
+class PostDetailView(generic.DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
