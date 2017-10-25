43a457cc7c6c6ba9644ced0e0d1d44a86054aabc
Marcus Crane <marcus@thingsima.de>
2017-10-25 15:14:12 +1300

    Cleaned up post rendering and confirmed that image rendering works

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ class PostView(generic.ListView):
 
   def get_queryset(self):
     """ Order posts by date in descending order."""
-    return Post.objects.filter(date__lte=timezone.now()).order_by('-date')[:3]
+    return Post.objects.filter(date__lte=timezone.now()).order_by('-date')
 
 class PostDetailView(generic.DetailView):
   model = Post
