5da25389616c18d714602203a80753d6a12161d4
Marcus Crane <marcus@thingsima.de>
2017-10-26 22:30:23 +1300

    Rearranged static

--- thingsimade/settings.py
+++ thingsimade/settings.py
@@ -118,4 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static'
+STATIC_URL = 'http://static.thingsima.de'
+
