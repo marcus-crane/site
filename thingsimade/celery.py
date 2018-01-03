f0660c41d30a77d437c593e93484d13360e49ef6
Marcus Crane <marcus@thingsima.de>
2018-01-03 23:42:45 +1300

    Setup celery scheduler

--- thingsimade/celery.py
+++ thingsimade/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thingsimade.settings')
@@ -9,6 +10,10 @@ app = Celery('thingsimade')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+	sender.add_periodic_task(30.0, stats.fetch_movies(), name='Fetch movies from Trakt')
+
+@app.task
+def test(arg):
+	print(arg)
