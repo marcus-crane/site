e3ef6fb771586084a1c9bbaabf52e30032a0856c
Marcus Crane <marcus@thingsima.de>
2018-01-03 22:48:36 +1300

    Review rendering works

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'reviews'
 urlpatterns = [
   path('', views.ReviewView.as_view(), name='list'),
-  # path('<slug:slug>/', views.PostDetailView.as_view(), name='detail'),
+  path('<slug:slug>/', views.ReviewDetailView.as_view(), name='detail'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -10,3 +10,12 @@ class ReviewView(generic.ListView):
   def get_queryset(self):
     """ Fetch only published posts, and order by descending date """
     return Review.objects.filter(date__lte=timezone.now()).order_by('-date')
+
+class ReviewDetailView(generic.DetailView):
+	template_name = 'reviews/detail.html'
+	model = Review
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['book_list'] = Review.objects.all()
+		return context
