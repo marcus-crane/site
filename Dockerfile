5388bcbf3e212fcc1cc88b3edb9be2f8e5f9bed8
Marcus Crane <marcus@thingsima.de>
2018-01-20 23:05:45 +1300

    Delete all files and start from scratch

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:3
-ENV PYTHONUNBUFFERED 1
-RUN mkdir /app
-WORKDIR /app
-ADD requirements.txt /app/
-RUN pip install -r requirements.txt
-RUN adduser --disabled-password --gecos '' dockeruser
-ADD . /app/
--- reviews/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'reviews'
-urlpatterns = [
-  path('', views.ReviewView.as_view(), name='list'),
-  path('<slug:slug>/', views.ReviewDetailView.as_view(), name='detail'),
-]
