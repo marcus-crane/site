4e3b1470f7c761ee46d6218ebaeb854d7e061a5d
Marcus Crane <marcus@thingsima.de>
2018-02-02 00:19:01 +1300

    Generated individual requirements files

--- stats/tasks.py
+++ stats/tasks.py
@@ -1,5 +1,4 @@
-import settings
-from utils import update_books, update_movies, update_music, update_shows
+import sources
 
 from celery import Celery
 
@@ -7,12 +6,7 @@ app = Celery('tasks', broker='amqp://guest:guest@rabbitmq:5672//')
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(21600, update_books.s(), name="Update books every 6 hours")
-    sender.add_periodic_task(14400, update_movies.s(), name="Update movies every 4 hours")
-    sender.add_periodic_task(300, update_music.s(), name="Update music every 5 minutes")
-    sender.add_periodic_task(1200, update_shows.s(), name="Update shows every 20 minutes")
-    sender.add_periodic_task(60, test.s('world'), name="Say world every 60 seconds")
-
-@app.task
-def test(arg):
-    print(arg)
+    sender.add_periodic_task(21600, sources.books.s(), name="Update books every 6 hours")
+    sender.add_periodic_task(14400, sources.movies.s(), name="Update movies every 4 hours")
+    sender.add_periodic_task(300, sources.music.s(), name="Update music every 5 minutes")
+    sender.add_periodic_task(1200, sources.shows.s(), name="Update shows every 20 minutes")
