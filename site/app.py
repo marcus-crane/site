d3c78cf8a20acda51338bff0dbac09272e36fbfd
Marcus Crane <marcus@thingsima.de>
2018-02-05 23:42:34 +1300

    Cleaned up files with pycharm

--- site/app.py
+++ site/app.py
@@ -1,36 +1,1 @@
-from flask import Flask, render_template
-
-import json
-from utils import get_post, get_posts, load_stats
-
-app = Flask(__name__, static_url_path='')
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/blog')
-def blog_list():
-	posts = get_posts('blog')
-	return render_template('blog/list.html', posts=posts)
-
-@app.route('/blog/<file>')
-def blog_detail(file):
-	post = get_post(file, 'blog')
-	return render_template('blog/detail.html', post=post)
-
-@app.route('/contact')
-def contact():
-	return render_template('contact.html')
-
-@app.route('/stats')
-def stats():
-    books = load_stats('books')
-    movies = load_stats('movies', 5)
-    music = load_stats('music', 5)
-    shows = load_stats('shows', 5)
-    return render_template('stats.html', books=books, movies=movies,
-        music=music, shows=shows)
-
-if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+from flask import Flask, render_templatefrom utils import get_post, get_posts, load_statsapp = Flask(__name__, static_url_path='')@app.route('/')def index():    return render_template('index.html')@app.route('/blog')def blog_list():    posts = get_posts('blog')    return render_template('blog/list.html', posts=posts)@app.route('/blog/<file>')def blog_detail(file):    post = get_post(file, 'blog')    return render_template('blog/detail.html', post=post)@app.route('/contact')def contact():    return render_template('contact.html')@app.route('/stats')def stats():    books = load_stats('books')    movies = load_stats('movies', 5)    music = load_stats('music', 5)    shows = load_stats('shows', 5)    return render_template('stats.html', books=books, movies=movies,                           music=music, shows=shows)if __name__ == '__main__':    app.run(host='0.0.0.0')
--- site/utils.py
+++ site/utils.py
@@ -6,31 +6,30 @@ import PyRSS2Gen
 import datetime
 import os
 
-def get_post(filename, dir):
-    def render_md(file):
-        data = meta.parse(file)
-        post = {}
-        post['title'] = data[0]['Title']
-        post['date'] = data[0]['Date']
-        post['sfw'] = data[0]['SFW']
-        post['content'] = mistune.markdown(data[1], escape=False)
-        return post
 
-    with open('posts/{}/{}.md'.format(dir, filename), 'r') as file:
+def get_post(filename, directory):
+    def render_md(post_file):
+        data = meta.parse(post_file)
+        content = mistune.markdown(data[1], escape=False)
+        post_data = dict(title=data[0]['Title'], date=data[0]['Date'],
+                         sfw=data[0]['SFW'], content=content)
+        return post_data
+
+    with open('posts/{}/{}.md'.format(directory, filename), 'r') as file:
         post = file.read()
         return render_md(post)
 
-def get_posts(dir):
-    path = os.listdir('posts/{}'.format(dir))
+
+def get_posts(directory):
+    path = os.listdir('posts/{}'.format(directory))
     posts = []
     # Remove the '.md' bit from the end of each file
     urls = [post[:-3] for post in path]
     for url in urls:
-        post = {}
-        post['title'] = url.replace('-', ' ')
-        post['slug'] = url
+        post = {'title': url.replace('-', ' '), 'slug': url}
         posts.append(post)
     return posts
+
 
 def generate_rss(section):
     posts = get_posts(section)
@@ -40,22 +39,23 @@ def generate_rss(section):
         item = get_post(slug, section)
         entries.append(
             PyRSS2Gen.RSSItem(
-                title = item['title'],
-                link = 'https://thingsima.de/blog/{}'.format(slug),
-                description = item['content'],
-                guid = 'https://thingsima.de/blog/{}'.format(slug),
-                pubDate = datetime.datetime(2003, 9, 6, 12, 00)))
+                title=item['title'],
+                link='https://thingsima.de/blog/{}'.format(slug),
+                description=item['content'],
+                guid='https://thingsima.de/blog/{}'.format(slug),
+                pubDate=datetime.datetime(2003, 9, 6, 12, 00)))
     rss = PyRSS2Gen.RSS2(
-        title = 'Things I Made',
-        link = 'https://thingsima.de/blog/',
-        description = 'Writing about things I find interesting!',
-        lastBuildDate = datetime.datetime.now(),
-        items = entries)
+        title='Things I Made',
+        link='https://thingsima.de/blog/',
+        description='Writing about things I find interesting!',
+        lastBuildDate=datetime.datetime.now(),
+        items=entries)
     rss.write_xml(open('static/rss.xml', 'w'))
 
-def load_stats(type, limit=None):
+
+def load_stats(media_type, limit=None):
     couch = couchdb.Server('http://couchdb:5984/')
-    media = couch[type]
+    media = couch[media_type]
     items = []
 
     for item in media:
